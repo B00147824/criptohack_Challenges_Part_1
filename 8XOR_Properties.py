3da37c94c8bdd58f95491deb77f00e5c9ce4420e
B00147824 <B00147824@mytudublin.ie>
2023-10-20 22:18:47 +0100

    9Favourite_byte

--- 8XOR_Properties.py
+++ 8XOR_Properties.py
@@ -1,6 +1,6 @@
 from pwn import xor	
 
-# Given values on the challenge question
+
 
 key1 = bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
 key1_2 = "37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e"
@@ -8,7 +8,7 @@ key2_3 = "c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1"
 flag_key123 = "04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf"
 
 
-# Solving for individual key values
+
 
 key2 = xor(bytes.fromhex(key1_2), key1)
 key3 = xor(bytes.fromhex(key2_3), key2)

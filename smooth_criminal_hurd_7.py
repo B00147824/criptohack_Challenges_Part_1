38a49de6c9f6fde29d899da0c1f20190035c449b
B00147824 <113989253+B00147824@users.noreply.github.com>
2023-12-29 10:59:46 +0000

    Update smooth_criminal_hurd_7.py

--- smooth_criminal_hurd_7.py
+++ smooth_criminal_hurd_7.py
@@ -1,4 +1,4 @@
-# Import necessary module
+
 from Crypto.Cipher import AES
 from Crypto.Util.number import inverse
 from Crypto.Util.Padding import pad, unpad
@@ -7,21 +7,19 @@ from random import randint
 import hashlib
 import os
 
-# Create a simple Point class to represent the affine points.
+
 Point = namedtuple("Point", "x y")
 
-# The point at infinity (origin for the group law).
 O = 'Origin'
 
-# Define the curve parameters
 p = 310717010502520989590157367261876774703
 a = 2
 b = 3
 
-# ... (other functions and code) ...
+
 
 def double_and_add(P: tuple, n: int):
-    # based on the algorithm in ICM
+ 
     Q = P
     R = O
     while n > 0:
@@ -32,4 +30,4 @@ def double_and_add(P: tuple, n: int):
     assert check_point(R)
     return R
 
-# ... (rest of the code) ...
+

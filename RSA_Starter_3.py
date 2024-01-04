af33ceb27604ba6340b4dd11fa37a5b689768d8d
B00147824 <113989253+B00147824@users.noreply.github.com>
2024-01-04 13:29:14 +0000

    Delete RSA_Starter_3.py

--- RSA_Starter_3.py
+++
@@ -1,9 +0,0 @@
-# Given values
-p = 857504083339712752489993810777
-q = 1029224947942998075080348647219
-
-# Calculate the totient (phi) of N
-phi_N = (p - 1) * (q - 1)
-
-# Print the result
-print(phi_N)
